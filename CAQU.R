e4771b52107624be996b120c2f828680128038cd
mbh038 <mike038@btinternet.com>
2015-04-25 12:54:00 +0100

    calculate diurnal means

---
+++ CAQU.R
@@ -0,0 +1,33 @@
+#Read in data, renamer columns and delete empty columns
+d<-read.csv("TNO2245Data_1429877310388.csv")
+colnames(d)<-c("TimeStamp","TP","PM10","PM2.5","PM1","T","H","WS","WD","GPS")
+d<-subset(d,select=-c(T,H,GPS))
+
+#retain only values where all PM data are valid
+dg<-subset(d,PM10 > 0 & PM2.5 > 0 & PM1 > 0)
+
+#Convert TimeStamp to POSIXlt format
+dg$TimeStamp<-as.POSIXlt(strptime(dg$TimeStamp, format= "%d/%m/%Y %T"))
+
+nr<-nrow(dg)
+
+#set up empty data frame for the mean values
+PM.means <- data.frame(Time= numeric(0), PMall=numeric(0), PM10=numeric(0),
+                       PM2_5=numeric(0),PM1=numeric(0),WS=numeric(0))
+
+
+times<-with(dg,paste(TimeStamp$hour,":",TimeStamp$min,sep=""))
+dg<-cbind(dg,times)
+daytimes<-unique(times)
+nt=length(daytimes)
+
+for (i in 1:nt){
+        PM.means[i,1]=i#as.character(daytimes[i])
+        for (j in 2:6){
+                PM.means[i,j]<-mean(subset(dg[,j],dg$times==daytimes[i]) )       
+        }
+        
+}
+
+ggplot(melt(PM.means,id.vars="Time"),aes(x=Time,y=value))+geom_point()+
+        facet_wrap(~variable,scales="free_y")
